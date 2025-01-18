7d33b4d84cacbbe965185ccb9a3bfecf74bdb813
Edmundoni <edmund.oni@gmail.com>
2025-01-18 22:04:55 +0000

    update

--- backend.tf
+++ backend.tf
@@ -1,11 +1,12 @@
 terraform {
+    required_version = ">= 1.0.0"
   backend "s3" {
     bucket         = "falcondevsecops01"
     region         = "eu-west-2"
     key            = "Github-Runner-TF/terraform.tfstate"
     encrypt        = true
   }
-  required_version = ">= 1.0.0"
+  
   required_providers {
     aws = {
       version = ">= 2.7.0"
